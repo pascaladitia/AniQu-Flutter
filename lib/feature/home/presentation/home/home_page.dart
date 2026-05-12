6cff68bfba7749e13c010975508b8470af293ac1
pascal <pascaladitia@gmail.com>
2026-05-12 20:41:58 +0700

    feat: implement home page UI with bloc integration and error handling

--- lib/feature/home/presentation/home/home_page.dart
+++ lib/feature/home/presentation/home/home_page.dart
@@ -1,4 +1,11 @@
+import 'package:aniqu_flutter/core/l10n/app_localizations.dart';
+import 'package:aniqu_flutter/core/widgets/app_error_dialog.dart';
+import 'package:aniqu_flutter/feature/home/data/models/anime/anime_item_model.dart';
+import 'package:aniqu_flutter/feature/home/presentation/home/home_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/di/injection.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +15,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()..addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (!_scrollController.hasClients) return;
+    if (_scrollController.position.pixels > _scrollController.position.maxScrollExtent - 500) {
+
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController
+    ..removeListener(_onScroll)
+    ..dispose();
+    super.dispose();
+  }
+
+  void _openDetail(AnimeItemModel animeItem) {
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (_) => sl<HomeCubit>()..loadInitial(),
+      child: Builder(
+        builder: (context) {
+          final l10n = AppLocalizations.of(context);
+          final colors = Theme.of(context).colorScheme;
+
+          return Scaffold(
+            backgroundColor: colors.surface,
+            body: RefreshIndicator(
+              onRefresh: () => context.read<HomeCubit>().loadInitial(),
+              child: BlocListener<HomeCubit, HomeState>(
+                listenWhen: (previous, current) => previous.error != current.error && current.error != null,
+                listener: (context, state) => showAppErrorDialog(context, message: state.error!),
+              ),
+            ),
+          );
+        }
+      ),
+    );
   }
+
 }

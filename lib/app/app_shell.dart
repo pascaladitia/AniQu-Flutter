f7eacdf090f4179e346bcad7c94f68e26067118f
pascal <pascaladitia@gmail.com>
2026-05-15 20:44:21 +0700

    feat: implement app shell with bottom navigation and configure dependency injection for home feature

--- lib/app/app_shell.dart
+++ lib/app/app_shell.dart
@@ -1,3 +1,5 @@
+import 'package:aniqu_flutter/core/l10n/app_localizations.dart';
+import 'package:aniqu_flutter/feature/home/presentation/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AppShell extends StatefulWidget {
@@ -8,8 +10,39 @@ class AppShell extends StatefulWidget {
 }
 
 class _AppShellState extends State<AppShell> {
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final l10n = AppLocalizations.of(context);
+    final pages = const [
+      HomePage()
+    ];
+
+    return Scaffold(
+      body: pages[_index],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _index,
+        onDestinationSelected: (value) => setState(() => _index = value),
+        destinations: [
+          NavigationDestination(
+            icon: const Icon(Icons.movie_creation_outlined),
+            label: l10n.home,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.search),
+            label: l10n.search,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.favorite_outline),
+            label: l10n.favorites,
+          ),
+          NavigationDestination(
+            icon: const Icon(Icons.settings_outlined),
+            label: l10n.settings,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,3 +1,9 @@
+import 'package:aniqu_flutter/feature/home/data/datasources/anime_remote_data_source.dart';
+import 'package:aniqu_flutter/feature/home/data/repositories/anime_repository_impl.dart';
+import 'package:aniqu_flutter/feature/home/domain/repositories/anime_repository.dart';
+import 'package:aniqu_flutter/feature/home/domain/usecases/get_anime_completed_usecase.dart';
+import 'package:aniqu_flutter/feature/home/domain/usecases/get_anime_ongoing_usecase.dart';
+import 'package:aniqu_flutter/feature/home/presentation/home/home_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../feature/settings/presentation/cubit/settings_cubit.dart';
@@ -10,4 +16,17 @@ Future<void> configureDependencies() async {
   sl.registerLazySingleton(() => AnimeClient.create().dio);
   sl.registerLazySingleton(() => PrefsManager());
   sl.registerLazySingleton(() => SettingsCubit(sl()));
+
+  sl.registerLazySingleton(() => AnimeRemoteDataSource(sl()));
+
+  sl.registerLazySingleton<AnimeRepository>(
+      () => AnimeRepositoryImpl(remote: sl())
+  );
+
+  sl.registerLazySingleton(() => GetAnimeOngoingUseCase(sl()));
+  sl.registerLazySingleton(() => GetAnimeCompletedUseCase(sl()));
+
+  sl.registerFactory(
+      () => HomeCubit(animeOngoingUseCase: sl(), animeCompletedUseCase: sl())
+  );
 }
